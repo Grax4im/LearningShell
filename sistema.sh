2b34d2669e1854d307b0f56fd026f701f46254c5
porao <ratomorto@riseup.net>
2020-02-04 01:15:36 -0300

    fix

---
+++ sistema.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# sistema - Script que mostra informações sobre o sistema
+# Autor - Grax4im
+
+ # O date mostra a data e a hora correntes
+ echo "Data e Horário:"
+ date
+ echo
+ # O df mostra as partições e quanto cada uma ocupa no disco
+ echo "Uso do disco:"
+ df
+ echo
+ # O w mostra os usuários que estão conectados nesta máquina
+ echo "Usuários conectados:"
+ w
