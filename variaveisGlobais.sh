7220830cfc435ccc7747bf9bcc394498419b5242
porao <ratomorto@riseup.net>
2020-02-12 13:55:20 -0300

    Printando argumentos de entrada

---
+++ variaveisGlobais.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+# Autor: Graxaim
+# Data: 12/02/2020
+# Desc: mostra na tela os argumentos de entrada com as variaveis globais $1 $2 $3...
+
+
+echo "O primeiro argumento é: $1"
+
+echo "O segundo argumento: $2"
+
+echo "O terceiro argumento: $3"
